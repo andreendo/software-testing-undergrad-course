a813132be07988726b79a0721b3f864fbb7000f2
andreendo <andreendo@gmail.com>
2020-03-09 16:04:31 -0300

    basic mocking project

---
+++ mockingProblemaInicial/test/inicial/ClienteTest.java
@@ -0,0 +1,15 @@
+package inicial;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClienteTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
